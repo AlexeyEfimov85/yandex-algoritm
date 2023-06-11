2df4d62872fa177f2abc590052387d3b01d86f04
Alexey Efimov <alex-vngd@yandex.ru>
2023-06-11 08:56:19 +0300

    работает

--- 05-find-vowels/script.js
+++ 05-find-vowels/script.js
@@ -8,9 +8,19 @@
  * ['а', 'я', 'о', 'ё', 'у', 'ю', 'ы', 'и', 'э', 'е'].
  *
 */
-
+const vowels = ['а', 'я', 'о', 'ё', 'у', 'ю', 'ы', 'и', 'э', 'е'];
 function findVowels(str) {
-    // Напишите код здесь
+    const arr = str.split('');
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let vowel = arr[i]
+        for (let i = 0; i < vowels.length; i++) {
+            if (vowel === vowels[i]) {
+                num = num + 1;
+            }
+
+        }
+    } return num;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
